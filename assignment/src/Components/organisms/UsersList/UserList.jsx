a672cdc7520ac896bb30a60c28d2bbff393448f8
Js Sanjay <mname8741@gmail.com>
2022-12-31 14:34:47 +0530

    loader

--- assignment/src/Components/organisms/UsersList/UserList.jsx
+++ assignment/src/Components/organisms/UsersList/UserList.jsx
@@ -4,18 +4,19 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getUsers } from '../../../Store/users/usersSlice'
 import UserCard from '../../molecules/UserCard/UserCard'
 import styles from "../UsersList/styles.module.scss"
+import Loader from "../../atoms/Loader/Loader"
 const UserList = () => {
   const dispatch=useDispatch()
-  const {users}=useSelector((store)=>store.users)
+  const {users,loading}=useSelector((store)=>store.users)
   console.log(users)
   useEffect(()=>{
     dispatch(getUsers())
   },[])
   return (
     <div className={styles.container}>
-      {users?.map((el)=>{
+      {!loading?users?.map((el)=>{
         return <UserCard key={el.id} data={el}/>
-      })}
+      }):<Loader/>}
     </div>
   )
 }
